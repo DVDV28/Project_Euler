b6887cf88879fc38208ee1d0bcfe3192483ad5cc
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2024-05-06 16:57:09 +1000

    Python environment management
    - All challenges print result
    - Can run challenges from cmd
    - Setup poetry for deps management
    - Setup ruff for linting, formatting

--- Python/solutions/002.py
+++ Python/solutions/002.py
@@ -2,14 +2,16 @@ def Fibgen(max_val):
     x1 = 1
     x2 = 2
     yield x2
-    counter = 0 # To make even values
+    counter = 0  # To make even values
 
-    while (x1 + x2 <= max_val):
+    while x1 + x2 <= max_val:
         x1 = x1 + x2
         counter += 1
-        if (counter == 3):
+        if counter == 3:
             yield x1
             counter = 0
         x1, x2 = x2, x1
 
-sum(Fibgen(4e6))
+
+res = sum(Fibgen(4e6))
+print(res)
--- Python/solutions/005.py
+++ Python/solutions/005.py
@@ -1,4 +1,4 @@
-# Didn't use a programmatic approach. 
+# Didn't use a programmatic approach.
 # The value is the product of the maximum power of the prime numbers in the list of numbers
 # This ends up as 2*2*2*2*3*3*5*7*11*13*17*19
 # Note that 'evenly divisible' refers to integer result, not even result
