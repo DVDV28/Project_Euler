554c71748ee6a7b4250503ba125fa0e226b35439
DVDV28 <70045260+DVDV28@users.noreply.github.com>
2024-05-26 22:06:20 +1000

    Solved question 9 in Python - existing script didn't solve it

--- Python/solutions/009.py
+++ Python/solutions/009.py
@@ -1,11 +1,19 @@
+# Find a Pythagorean triple such that
 # a + b + c = 1000
-# (a + b + c)**2 = 1000000
-# a^2 + b^2 + c^2 + 2ab + 2ac + 2bc = 1000000
-# a^2 + b^2 + 2ab + 1 - a - b = 499001
-# (a + b)^2 - (a + b)  - 499000 = 0
 
-prod = 1
-for x in [5, 5, 7, 6, 6, 8, 9, 6, 6, 4, 8, 9, 5]:
-    prod *= x
+## This solution uses Euclid's formula for generating Pythagorean triples
 
-print(prod)
+m = 2
+n = 1
+
+while True:
+    for n in range(1, m):
+        a = m**2 - n**2
+        b = 2 * m * n
+        c = m**2 + n**2
+
+        if a + b + c == 1000:
+            print(a * b * c)
+            break
+
+    m += 1
