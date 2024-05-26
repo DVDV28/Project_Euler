ec582b13b56ae382102226f55a6efe5d347d0e27
DVDV28 <70045260+DVDV28@users.noreply.github.com>
2024-05-26 17:15:03 +1000

    Created supporting function which returns a vector of all prime factors of a number. When there are duplicates (e.g. 4 appears twice) then the prime appears twice.

--- R/Prime Functions.R
+++ R/Prime Functions.R
@@ -17,3 +17,39 @@ is.prime <- function(n) {
     }
     return(T)
 }
+
+get_prime_divisors <- function(n) {
+    ## Recycles code from solution to Q10: create a prime sieve
+    number_seq <- 1:n
+    is_prime_seq <- rep(T, n)
+    
+    is_prime_seq[1] <- F
+    
+    div <- 2
+    while (div <= sqrt(n) & !is.na(div)) {
+        is_prime_seq[seq(div^2, n, by = div)] <- F ## squared, because the first instance is prime and the nth instance is handled elsewhere
+        
+        div <- number_seq[number_seq > div & is_prime_seq][1]
+    }
+    
+    primes <- number_seq[is_prime_seq]
+    powers <- rep(0, length(primes))
+    
+    i <- 1
+    while (i <= length(primes)) {
+        if (n %% primes[i] == 0) {
+            n <- n / primes[i]
+            powers[i] <- powers[i] + 1
+        } else {
+            i <- i + 1
+        }
+        
+        if (n == 1) {
+            break()
+        }
+    }
+    
+    divisors <- unlist(mapply(rep, primes, powers))
+    
+    return(divisors)
+}
