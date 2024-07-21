df8cb1d3a87cb3e2be1d888a4228dd5830d2c0e6
DVDV28 <70045260+DVDV28@users.noreply.github.com>
2024-07-21 18:51:21 +1000

    Added commentary

--- Solution Scripts/024.R
+++ Solution Scripts/024.R
@@ -4,6 +4,9 @@
 ## 012   021   102   120   201   210
 ## What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+## This solution identifies which digit should be slotted in each one based on relativities
+## Once that is done, it moves from number to number, eliminating numbers already used
+
 position <- 0
 target <- 1e6
 
